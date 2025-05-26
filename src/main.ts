f0b697e0568d346b12f0f5e80157066a2e87e31c
Alvaro <alvaro11122@gmail.com>
2025-05-26 18:41:14 -0300

    PWA

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { AppComponent } from './app/app.component';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 import localeEsAR from '@angular/common/locales/es-AR';
-import { LOCALE_ID } from '@angular/core';
+import { LOCALE_ID, isDevMode } from '@angular/core';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import 'zone.js';
+import { provideServiceWorker } from '@angular/service-worker';
 
 registerLocaleData(localeEs, 'es-ES');
 registerLocaleData(localeEsAR, 'es-AR');
@@ -16,6 +17,9 @@ bootstrapApplication(AppComponent, {
   providers: [
     ...(appConfig.providers || []),
     { provide: LOCALE_ID, useValue: 'es-ES' }, // Para fechas y pipes
-    { provide: MAT_DATE_LOCALE, useValue: 'es-ES' }, // Para datepicker en español
+    { provide: MAT_DATE_LOCALE, useValue: 'es-ES' }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), // Para datepicker en español
   ],
 }).catch((err) => console.error(err));
